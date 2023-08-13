f0e3023282143dba308602d3992fca6b7311c1e1
gk7734 <gamekiller7734@naver.com>
2023-08-13 20:17:49 +0900

    확인 테스트

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -151,7 +151,7 @@ function About() {
                         <div className='grid grid-cols-2 md:grid-cols-4 xl:flex xl:gap-x-6'>
                             {/*experience*/}
                             <div className='relative flex-1 after:w-[1px] after:h-full
-                            after:bg-white/10 after:absolute after:top-0 after:right-0 md:after:invisible'>
+                            after:bg-white/10 after:absolute after:top-0 after:right-0'>
                                 <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
                                     <CountUp start={0} end={10}  duration={5}/> +
                                 </div>
@@ -159,7 +159,7 @@ function About() {
                             </div>
                             {/*clients*/}
                             <div className='relative flex-1 after:w-[1px] after:h-full
-                            after:bg-white/10 after:absolute after:top-0 after:right-0 md:after:invisible'>
+                            after:bg-white/10 after:absolute after:top-0 after:right-0'>
                                 <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
                                     <CountUp start={0} end={250}  duration={5}/> +
                                 </div>
@@ -167,14 +167,14 @@ function About() {
                             </div>
                             {/*projects*/}
                             <div className='relative flex-1 after:w-[1px] after:h-full
-                            after:bg-white/10 after:absolute after:top-0 after:right-0 md:after:visible'>
+                            after:bg-white/10 after:absolute after:top-0 after:right-0'>
                                 <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
                                     <CountUp start={0} end={650}  duration={5}/> +
                                 </div>
                                 <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Finished projects</div>
                             </div>
                             {/*awards*/}
-                            <div className='relative flex-1 md:after:hidden'>
+                            <div className='relative flex-1'>
                                 <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
                                     <CountUp start={0} end={8}  duration={5}/> +
                                 </div>
