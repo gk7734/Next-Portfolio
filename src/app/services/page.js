92e7005753e8ae1bfdd20d78e4a1b819f3d555e0
gk7734 <gamekiller7734@naver.com>
2023-08-14 21:11:57 +0900

    Service 페이지

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -18,21 +18,28 @@ function Services() {
                     {/*text*/}
                     <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
                         <motion.h2
-                            variants={fadeIn('up', 0.3)}
+                            variants={fadeIn('up', 0.2)}
                             initial='hidden'
                             animate='show'
                             exit='hidden'
                             className='h2 xl:mt-8'>
                             My services <span className='text-accent'>.</span>
                         </motion.h2>
-                        <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ea
-                            excepturi iure molestias nostrum, obcaecati officia sunt temporibus veritatis voluptates.</p>
+                        <motion.p variants={fadeIn('up', 0.4)}
+                                  initial='hidden'
+                                  animate='show'
+                                  exit='hidden'
+                                  className='mb-4 max-w-[400px] mx-auto lg:mx-0'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ea
+                            excepturi iure molestias nostrum, obcaecati officia sunt temporibus veritatis voluptates.</motion.p>
                     </div>
-                    <div className='w-full xl:max-w-[65%]'>
-                        {/*slider*/}
+                    {/*slider*/}
+                    <motion.div variants={fadeIn('down', 0.6)}
+                                initial='hidden'
+                                animate='show'
+                                exit='hidden'
+                                className='w-full xl:max-w-[65%]'>
                         <ServiceSlider />
-                    </div>
-
+                    </motion.div>
                 </div>
             </div>
             <Bulb />
